f058212c2dd10094e44b8a45b925ed60bda8f58e
Jonathan Bareket <jonathan.bareket@mgb20041-imac27.ber.s-f.com>
2018-09-14 18:04:27 +0200

    .gitignore is now working

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ else{
     client_id =   process.env.CLIENT_ID; // Your client id
     client_secret =  process.env.CLIENT_SECRET; // Your secret
     redirect_uri = process.env.REDIRECT_URI; // Your redirect uri
-    client_url = 'https://react-express-j.herokuapp.com/#';
+    client_url = 'https://spot-stats.herokuapp.com/#';
 }
 
 var generateRandomString = function(length) {
