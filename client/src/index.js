974e66780c20dc485eaaa420c4623441adccb6d9
Jonathan Bareket <jonathan.bareket@mgb20041-imac27.ber.s-f.com>
2018-09-19 15:22:18 +0200

    added jquery to client index.html, cleared unused vars

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 // import { BrowserRouter } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+
 
 import './index.css';
 import App from './App';
 
 render((
-  <CookiesProvider>
         <App/>
-        </CookiesProvider>
 ), document.getElementById('root'));
