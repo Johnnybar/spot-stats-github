c86e8411d77a53c81117d68bb81f0af6ca451844
Jonathan Bareket <johnnybareket@gmail.com>
2018-10-26 10:17:30 +0200

    replaced font, removed duplicate subscribe section

--- client/src/App.js
+++ client/src/App.js
@@ -288,25 +288,6 @@ class App extends Component {
       </div>
     </section>
 
-    {/* <!-- Signup Section --> */}
-    <section id="signup" className="signup-section">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-10 col-lg-8 mx-auto text-center">
-
-            <i className="far fa-paper-plane fa-2x mb-2 text-white"></i>
-            <h2 className="text-white mb-5">Subscribe to receive updates!</h2>
-
-            <form className="form-inline d-flex">
-              <input type="email" className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address..."></input>
-              <button type="submit" className="btn btn-primary mx-auto">Subscribe</button>
-            </form>
-
-          </div>
-        </div>
-      </div>
-    </section>
-
     {/* <!-- Contact Section --> */}
     <section className="contact-section bg-black">
       <div className="container">
